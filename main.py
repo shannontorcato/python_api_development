32a4630dbc3564beab2a0990cf080ed822a6080b
shannontorcato <shannon.torcato1234@gmail.com>
2022-12-23 14:40:14 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from typing import Optional
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+from random import random, randrange
+
+app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+my_posts = [{"title":"title of post 1", "content":"content of post 1", "id":1}, {"title":"title of post 2", "content":"content of post 2", "id":2}]
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, welcome to Billys World"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": my_posts}
+
+@app.post("/posts")
+def create_posts(post: Post):
+    new_post = post.dict()
+    new_post['id'] = randrange(1,100000)
+    my_posts.append(new_post)
+    return {"data": post}
+# title str, content str
